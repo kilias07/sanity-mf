be2e8b66c4f02c671756469b3ee3f0d685c0014f
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2022-06-03 09:13:36 +0200

    add google Geopoints

--- schemas/geoPoints.js
+++ schemas/geoPoints.js
@@ -16,6 +16,36 @@ export default {
             description: "Nazwa salonu",
         },
         {
+            name: "address",
+            title: "Address",
+            type: "array",
+            of: [
+                {
+                    type: "object",
+                    fields: [
+                        {
+                            name: "streetName",
+                            type: "string",
+                            title: "Street Name",
+                            description: "Nazwa ulicy i numer",
+                        },
+                        {
+                            name: "postalCode",
+                            type: "string",
+                            title: "Postal Code",
+                            description: "Kod pocztowy",
+                        },
+                        {
+                            name: "cityName",
+                            type: "string",
+                            title: "City name",
+                            description: "Nazwa miasta",
+                        }
+                    ]
+                }
+            ]
+        },
+        {
             name: "featuredImage",
             type: "image",
             title: "Featured Image",
